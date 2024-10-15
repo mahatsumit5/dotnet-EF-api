b6890b66a735130420444b117d338c796b61ae5b
mahatsumit5 <mahatsumit5@gmail.com>
2024-10-15 21:09:35 +1100

    "Updated AssemblyInfo.cs and AssemblyInfoInputs.cache files with new version information."

--- obj/Debug/net8.0/DotnetApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/DotnetApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotnetApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+398bcb585916a896192ccd3be032cf82158bc090")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31603b7c768c2c38a6faf757ec748fc728d85234")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotnetApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotnetApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
